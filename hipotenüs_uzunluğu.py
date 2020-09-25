19e6d614e74f9b1fb3a856b2addf022b323069b5
sahintuter <71895937+sahintuter@users.noreply.github.com>
2020-09-25 22:26:02 +0300

    Update hipotenüs_uzunluğu.py

--- hipotenüs_uzunluğu.py
+++ hipotenüs_uzunluğu.py
@@ -1,7 +1,7 @@
 
 print("Hipotenüs uzunluğunu bulalım")
 
-a  =  int ( input ( "a dik kenerı :" )) #kullanıcıdan 1. dik kenarı istiyoruz
+a  =  int ( input ( "a dik kenarı :" )) z#kullanıcıdan 1. dik kenarı istiyoruz
 
 b  =  int ( input ( "b dik kenarı :" )) #kullanıcıdan 2. dik kenarı istiyoruz
 
