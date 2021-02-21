8419a9f3eb6e90e77e398415037ea38988baf57a
sahintuter <71895937+sahintuter@users.noreply.github.com>
2021-02-21 09:49:32 +0300

    Making a heart with the Turtle module

---
+++ kalp.py
@@ -0,0 +1,42 @@
+import turtle
+
+turtle.speed(0)
+
+turtle.bgcolor("pink")
+
+
+
+def donus():
+
+    for i in range(200):
+
+        turtle.right(1)
+
+        turtle.forward(1)
+
+
+
+turtle.color("red")
+
+
+
+turtle.begin_fill()
+
+turtle.left(140)
+
+turtle.forward(111.65)
+
+donus()
+
+
+
+turtle.left(120)
+
+donus()
+
+turtle.forward(111.65)
+
+turtle.end_fill()
+
+turtle.hideturtle()
+turtle.done()
